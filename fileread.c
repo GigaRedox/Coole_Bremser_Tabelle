7a77063f06a99ff19a8c76c80b241f843578b142
GigaRedox <abzaouigabriel@gmail.com>
2025-04-14 01:31:12 +0200

    table was changed to be dynamically allocated (option to add rowes and columns needs to be added), in the processes alignment of the percentages got broken (needs to be fixed)

--- fileread.c
+++ fileread.c
@@ -1,15 +1,18 @@
 #include <complex.h>
 #include <stddef.h>
-#define ROWS 3
-#define COLS 2
+#include <sys/types.h>
+int ROWS;//=3;
+int COLS;//=2;
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 int con=1;
-size_t len_max[COLS+1]={0,0,0};
-char table[ROWS][COLS+1][100];
+size_t* len_max;//[COLS+1]={0,0,0};
+//char table[ROWS][COLS+1][100];
+char*** table;
 char input[100];
-void normalize_stringlength(char a[100],int j){
+double* percentages_col;//=calloc(ROWS, sizeof(char*));
+void normalize_stringlength(char* a,int j){
     int len = strlen(a);  // Get the current length of the string
     if (len < len_max[j]) {
         int spaces_to_add = len_max[j] - len;  // Calculate how many spaces to add
@@ -23,14 +26,24 @@ void normalize_stringlength(char a[100],int j){
 void max_length(int COL){
     len_max[COL]=0;
     for (int i=0; i<ROWS; i++) {
-        if (strlen(table[i][COL])>len_max[COL]) {
-            len_max[COL]=strlen(table[i][COL]);
+        if (COL<COLS) {
+                if (strlen(table[i][COL])>len_max[COL]) {
+                len_max[COL]=strlen(table[i][COL]);
+            }
+        }
+        else {
+            char buffer[100];
+            sprintf(buffer,"%.2f%%", percentages_col[i]);
+            if (strlen(buffer)>len_max[COL]) {
+                len_max[COL]=strlen(buffer);
+        
+            }
         }
     
     }
 }
 void max_lengths(){
-    for (int i=0; i<COLS+1; i++) {
+    for (int i=0; i<COLS; i++) {
         max_length(i);
     }
 }
@@ -55,14 +68,23 @@ void trim_table(){
         }
     }
 }
-void printtable(){
+void printtable(){//!needs to be changed include headline with labels and convert percentages into strings and print them as (virtual column) table[i][COLS] !!! last cols is actually COLS-1
     trim_table();
     max_lengths();
     for (int i=0; i<ROWS; i++) {
-        for (int j=0; j<COLS+1; j++) {
-            normalize_stringlength(table[i][j],j);
-            printf("%s|",table[i][j]);
-            
+        for (int j=0; j<COLS+1; j++) {// note only prints really exiting rows and cols
+            if(j<COLS){
+                normalize_stringlength(table[i][j],j);
+                printf("%s|",table[i][j]);
+            }
+            else {
+                char* buffer=calloc(1024, sizeof(char));
+                sprintf(buffer,"%.2f%%", percentages_col[i]);
+                normalize_stringlength(buffer,j);
+                printf("%s|",buffer);
+                free(buffer);
+                
+            }    
             
         }
         printf("\n");
@@ -79,7 +101,8 @@ void percentage(){
             count++;
         }
     //table[i][COLS]=sum[i];
-        sprintf(table[i][COLS], "%.2f%%",(sum/(40*count))*100);
+        percentages_col[i]=(sum/(40*count))*100;
+        //sprintf(table[i][COLS], "%.2f%%",(sum/(40*count))*100);
         
         
     }
@@ -113,15 +136,103 @@ void save_file(char* name){
     }
     fclose(saved_file);
 }
+void number_of_rows(char* file_name){
+    int num_of_rows=0;
+    char buffer[1024];
+    FILE* file=fopen(file_name, "r");
+    while(fgets(buffer,1024*sizeof(char),file)!=NULL){
+        num_of_rows++;
+    }
+    fclose(file);
+    ROWS=num_of_rows;
+}
+void number_of_cols(char* file_name){
+    int num_of_cols=0;               
+    char buffer[1024];
+    char* token;
+    FILE* file=fopen(file_name, "r");
+    fgets(buffer, sizeof(buffer), file);
+    token=strtok(buffer, ",");
+    while (token!=NULL) {
+        num_of_cols++;
+        token=strtok(NULL, ",");
+    }
+    COLS=num_of_cols;
+    
+}
+void allocate_table(){
+    table=calloc(ROWS,sizeof(char*));
+    for (int i=0; i<ROWS; i++) {
+        table[i]=calloc(COLS, sizeof(char*));
+        for (int j=0;j<COLS; j++) {
+            table[i][j]=calloc(1024, sizeof(char));
+        }
+    }
+    
+}
+void add_table_row(){
+    //ROWS++;
+    char*** temp=realloc(table, ROWS*sizeof(char*));
+    table=temp;
+    table[ROWS-1]=calloc(COLS, sizeof(char*));
+    for(int i=0;i<COLS;i++){
+        table[ROWS-1][i]=calloc(1024, sizeof(char));
+        table[ROWS-1][i]="0";
+    }
+
+}
+void add_table_col(){
+    //COLS++;
+    char** temp;
+    for (int i=0; i<ROWS; i++) {
+        temp=realloc(table[i], COLS);
+        table[i]=temp;
+        table[i][COLS-1]=calloc(1024, sizeof(char));
+        table[i][COLS-1]="0";
+    }
+}
+void free_table(){
+    for (int i=0; i<ROWS; i++) {
+        for (int j=0; j<COLS; j++) {
+            free(table[i][j]);
+        }
+        free(table[i]);
+    }
+    free(table);
+}
+void allocate_percentages_col(){
+    percentages_col=calloc(COLS, sizeof(double));
+    
+}
+void add_percentages_col_col(){
+    //cant do COLS++; because COLS is a global variable that influences multiple pointers
+    // before call need to increment COLS by one 
+    double* temp=realloc(percentages_col, COLS);//effevtively COLS+1
+    percentages_col[COLS]=0;
+}
+void free_percentages_cols(){
+    free(percentages_col);
+}
+void allocate_len_max(){
+    len_max=calloc(COLS, sizeof(size_t));
+}
+void add_len_max_col(){
+    size_t* temp=realloc(len_max, COLS+1);//again effectively COLS+1
+    len_max=temp;
+    len_max[COLS]=0;
+}
+void free_len_max(){
+    free(len_max);
+}
 void load(char* file_name){
 FILE* ptr=fopen(file_name,"r");
-    char str[100];
+    char str[1024];
 
     //char lines[ROWS][100];
     char* a;
     size_t len;
     for(int i=0;i<ROWS;i++){
-        fgets(str,100,ptr);
+        fgets(str,sizeof(str),ptr);
         len=strlen(str);
         str[len-1]='\0';
         for (int j=0;j<COLS; j++) {
@@ -141,10 +252,15 @@ int main(){
     printf("Which file should be loaded?\n");
     fgets(file_name,100,stdin);
     file_name[strcspn(file_name,"\n")]='\0';
+    number_of_cols(file_name);
+    number_of_rows(file_name);
+    allocate_len_max();
+    allocate_percentages_col();
+    allocate_table();
     load(file_name);
     percentage();
     printtable();
-    while(con){//while con==true <=> con=1
+    while(con){//while con==true <=> con==1
     printf("What would you like to do? \n");
     fgets(input,sizeof(input),stdin);
     //trim_string(input);
@@ -179,6 +295,12 @@ int main(){
             new_vals[i][strcspn(new_vals[i], "\n")]='\0';
         }
         change_values(rows,cols,new_vals,number_of_vals);
+        free(rows);
+        free(cols);
+        for (int i=0; i<number_of_vals; i++) {
+            free(new_vals[i]);
+        }
+        free(new_vals);
         trim_table();
         percentage();
         printtable();
